bcfe3bd254e45e57b546b0a6273d64d27b58a2d5
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2022-08-16 19:11:05 -0300

    Cria contextda api

---
+++ Server/Server-pdi/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿namespace Entities
+{
+    public class User : BaseEntities
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public AccessType Role { get; set; }
+        public string Token { get; set; }
+    }
+
+
+
+
+
+    public enum AccessType
+    {
+        ADM,
+        USER,
+        NULL
+    }
+}
